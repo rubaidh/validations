0358947fd923e1a05d0525379b69d5fb5711692e
Graeme Mathieson <mathie@woss.name>
2008-12-04 13:23:21 +0000

    Add in the start of a strict validation for URLs.

--- lib/rubaidh/validations.rb
+++ lib/rubaidh/validations.rb
@@ -10,11 +10,19 @@ module Rubaidh
         EmailAddressLocalPartQuoted = '[\x00-\xFF]+'.freeze
         EmailAddressLocalPart = "(#{EmailAddressLocalPartUnquoted}|\"#{EmailAddressLocalPartQuoted}\")".freeze
         EmailAddress = "#{EmailAddressLocalPart}@#{FullyQualifiedDomainName}".freeze
+
+        Protocol = "https?".freeze
+        OptionalPort = "(:[0-9]{1,5})?".freeze
+        OptionalPath = "([[:alnum:]._~/-]|%[0-9a-fA-F]+)*"
       end
 
       Hostname = /\A#{RegexStr::Hostname}\Z/.freeze
       FullyQualifiedDomainName = /\A#{RegexStr::FullyQualifiedDomainName}\Z/.freeze
       EmailAddress = /\A#{RegexStr::EmailAddress}\Z/.freeze
+
+      # FIXME: We should be checking for query strings and fragments here, but
+      # pragmatism has won out today.
+      Url = /\A#{RegexStr::Protocol}:\/\/#{RegexStr::FullyQualifiedDomainName}#{RegexStr::OptionalPort}#{RegexStr::OptionalPath}/.freeze
     end
 
     def self.included(base)
@@ -45,6 +53,13 @@ module Rubaidh
       def validates_email_address_format_of(*attr_names)
         options = attr_names.extract_options!
         configuration = { :with => Rubaidh::Validations::RegularExpressions::EmailAddress }
+        configuration.update(options)
+        validates_format_of *(attr_names + [configuration])
+      end
+
+      def validates_url_format_of(*attr_names)
+        options = attr_names.extract_options!
+        configuration = { :with => Rubaidh::Validations::RegularExpressions::Url }
         configuration.update(options)
         validates_format_of *(attr_names + [configuration])
       end
